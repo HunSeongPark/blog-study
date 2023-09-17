73ba2ed52f58802b44c87d29b44bbcecc9c64046
HunSeongPark <phunseong@gmail.com>
2023-09-17 14:59:05 +0900

    chore: querydsl 추가

---
+++ querydsl-in-empty-list/src/main/java/com/example/querydslinemptylist/QuerydslInEmptyListApplication.java
@@ -0,0 +1,13 @@
+package com.example.querydslinemptylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslInEmptyListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslInEmptyListApplication.class, args);
+	}
+
+}
---
+++ querydsl-in-empty-list/src/test/java/com/example/querydslinemptylist/QuerydslInEmptyListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.querydslinemptylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslInEmptyListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
