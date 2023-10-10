fc5c0769cb07139112f73eb07abba34afd931fc9
HunSeongPark <phunseong@gmail.com>
2023-10-10 21:55:48 +0900

    chore: feign-client-retry initial commit

---
+++ feign-client-retry/internal-server/src/main/java/com/example/internalserver/InternalServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.internalserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternalServerApplication.class, args);
+	}
+
+}
---
+++ feign-client-retry/internal-server/src/test/java/com/example/internalserver/InternalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internalserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
