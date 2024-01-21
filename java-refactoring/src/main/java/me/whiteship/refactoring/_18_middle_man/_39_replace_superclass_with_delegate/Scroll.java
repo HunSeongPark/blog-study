1c2466eec49606a9fcf8bfe0e8fbcd68e335ffe2
HunSeongPark <phunseong@gmail.com>
2024-01-21 18:26:08 +0900

    자바 리팩토링 - 상속을 위임으로 바꾸기

--- java-refactoring/src/main/java/me/whiteship/refactoring/_18_middle_man/_39_replace_superclass_with_delegate/Scroll.java
+++ java-refactoring/src/main/java/me/whiteship/refactoring/_18_middle_man/_39_replace_superclass_with_delegate/Scroll.java
@@ -4,16 +4,25 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 
-public class Scroll extends CategoryItem {
+public class Scroll {
 
     private LocalDate dateLastCleaned;
+    private CategoryItem categoryItem; // 필드 선언
 
     public Scroll(Integer id, String title, List<String> tags, LocalDate dateLastCleaned) {
-        super(id, title, tags);
         this.dateLastCleaned = dateLastCleaned;
+        this.categoryItem = new CategoryItem(id, title, tags);
     }
 
     public long daysSinceLastCleaning(LocalDate targetDate) {
         return this.dateLastCleaned.until(targetDate, ChronoUnit.DAYS);
+    }
+
+    public LocalDate getDateLastCleaned() {
+        return dateLastCleaned;
+    }
+
+    public Integer getId() {
+        return this.categoryItem.getId(); // 해당 코드만 수정하면 됨
     }
 }
